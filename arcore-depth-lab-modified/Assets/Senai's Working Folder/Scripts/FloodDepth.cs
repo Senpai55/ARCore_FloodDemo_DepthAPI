51cd125994ab4919057f67fe6a3eeca5a36725fe
Senai <senai.m.lk@gmail.com>
2020-09-25 10:39:28 +1200

    First Upload

---
+++ arcore-depth-lab-modified/Assets/Senai's Working Folder/Scripts/FloodDepth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class FloodDepth : MonoBehaviour
+{
+    //dynamic float that is updated by slider value
+    public float waterGridHeight;
+
+    //slider that enables changing flood level
+    public Slider floodLevelSlider;
+
+    //gameobject controlling rain particles
+    public GameObject rain;
+
+    //set water grid height
+    public void SetDepth(float value)
+    {           
+        //update flood height based on slider value 
+        //which can then be passed into the WaterGridManager script to update WaterDepthInM value
+        waterGridHeight = value;
+    }
+
+    public void Flood()
+    {
+        //increase rain
+        rain.SetActive(true);
+
+        //raise flood level over 5 seconds
+        floodLevelSlider.DOValue(floodLevelSlider.maxValue, 5);
+    }
+
+    public void Drain()
+    {
+        //stop rain
+        rain.SetActive(false);
+
+        //lower flood level over 5 seconds
+        floodLevelSlider.DOValue(floodLevelSlider.minValue, 5);
+    }
+}
